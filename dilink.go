bd52f2c7d90fc8274c5aa577d6633c0392d76cce
psh845 <d10438@idea.co.kr>
2018-06-04 15:39:39 +0900

    usd 타입 추가

--- dilink.go
+++ dilink.go
@@ -88,7 +88,7 @@ func installDilink() {
 func runWin(scape string) {
 	switch strings.ToLower(filepath.Ext(scape)) {
 	case ".mov":
-		os.Setenv("RV_SUPPORT_PATH", "//10.0.200.100/_lustre_INHouse/rv/supportPath")                            // 회사 RV 파이프라인툴을 로딩하기 위해서 필요하다.
+		os.Setenv("RV_SUPPORT_PATH", "//10.0.200.100/_lustre_INHouse/rv/supportPath") // 회사 RV 파이프라인툴을 로딩하기 위해서 필요하다.
 		if strings.Contains(scape, ";") {
 			var movlist []string
 			pathlist := strings.Split(scape, ";")
@@ -107,7 +107,7 @@ func runWin(scape string) {
 		}
 		return
 	case ".rv":
-		os.Setenv("RV_SUPPORT_PATH", "//10.0.200.100/_lustre_INHouse/rv/supportPath")                            // 회사 RV 파이프라인툴을 로딩하기 위해서 필요하다.
+		os.Setenv("RV_SUPPORT_PATH", "//10.0.200.100/_lustre_INHouse/rv/supportPath") // 회사 RV 파이프라인툴을 로딩하기 위해서 필요하다.
 		err := exec.Command(RV_WIN, dipath.Lin2win(scape)).Run()
 		if err != nil {
 			log.Fatal(err)
@@ -279,6 +279,12 @@ func runLin(scape string) {
 	case ".hip":
 		syscall.Umask(0002) // 윈도우는 지원 안함.
 		err := exec.Command("gnome-terminal", "-x", "h", scape).Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+	case ".usd", ".usda":
+		syscall.Umask(0002) // 윈도우는 지원 안함.
+		err := exec.Command("gnome-terminal", "-x", "uview", scape).Run()
 		if err != nil {
 			log.Fatal(err)
 		}
