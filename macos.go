d7a73cbcfa9aef4d47b30d506ad9d83a816a6588
hanwoong kim <hello@lazypic.org>
2019-11-21 23:23:14 +0900

    add djv

--- macos.go
+++ macos.go
@@ -30,9 +30,9 @@ func MacOS(scape string) {
 		if err != nil {
 			log.Fatal(err)
 		}
-	case ".mov", ".jpg":
+	case ".mov", ".jpg": // DJV2 를 사용한다.
 		os.Setenv("RV_ENABLE_MIO_FFMPEG", "1") // Prores 코덱을 위해서 활성화 한다.
-		err := exec.Command(rvMacosAppPath, scape).Run()
+		err := exec.Command("/Applications/DJV2.app/Contents/MacOS/DJV2", scape).Run()
 		if err != nil {
 			log.Fatal(err)
 		}
